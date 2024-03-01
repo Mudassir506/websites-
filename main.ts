b09142de7732a0e5d21ea324da03d5c2ccc75948
Mudassir506 <71308178+Mudassir506@users.noreply.github.com>
2024-03-01 23:19:42 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,83 @@
+// arethimatic operaters
+
+let num1 = 25;
+let num2 = 30;
+ console.log(num1 + num2);
+
+console.log(3+3);
+
+let num1 = 6;
+let num2 = 5;
+let sum = num1 + num2 + 5;
+console.log(sum);
+
+let num1 = 10;
+let num2 = 6;
+console.log(num1-num2);
+
+let num1 = 10;
+let num2 = 6;
+console.log(num2-num1);
+
+let num1 = 10;
+let num2 = 6;
+console.log(num1*num2);
+
+let num1 = 10;
+let num2 = 6;
+console.log(num1%num2);
+
+// concatenation
+
+let firstName = "Mudassir";
+let lastName = "Khan";
+
+console.log(firstName+lastName);
+
+let firstName = "Mudassir";
+let lastName = "Khan";
+
+console.log(firstName +" "+ lastName);
+
+let num1 : number = 10;
+let num2 : string = "22";
+
+console.log(num1+num2);
+
+//assigning operaters
+
+let n = 5;
+n = n+1;
+console.log(n);
+
+let n = 5;
+n +=1 ;
+console.log(n);
+
+let n = 5;
+n -=1 ;
+console.log(n);
+
+ let n = 5;
+ n *=2;
+ console.log(n);
+
+// comparison operaters
+let n = 5;
+let a = 5;
+console.log(n===a);
+
+
+let n = 5;
+let a = 6;
+console.log(n>=a);
+
+let n = 5;
+let a = 6;
+console.log(n==a);
+ 
+// not equal to operater conditions 
+
+let n = 10;
+console.log(n !=10);
+
